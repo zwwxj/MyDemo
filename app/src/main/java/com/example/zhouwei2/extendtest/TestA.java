60c9f1c8a89dcb2ba710bb0ef3c35cfe2ed2db2f
zhouwei2 <794293764@qq.com>
2019-01-08 16:35:58 +0800

    回滚提交

--- app/src/main/java/com/example/zhouwei2/extendtest/TestA.java
+++ app/src/main/java/com/example/zhouwei2/extendtest/TestA.java
@@ -19,6 +19,7 @@ public class TestA {
     public void A(){
         Log.e("测试A:","这是A类的方法A");
     }
+<<<<<<< HEAD
 	
 	public void combine(){
 		Log.e("测试A:","合并的测试");
@@ -27,4 +28,6 @@ public class TestA {
 	public void new_branch(){
 		Log.e("test","new_branch")
 	}
+=======
+>>>>>>> parent of 9e8aad8... add  dev   daima
 }
